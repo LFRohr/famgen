eeb7fbadd0e797d79b440bd180d8f5a29720b659
Larry Rohr <lfrohr@gmail.com>
2016-07-20 15:04:43 -0700

    Add background

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
   #root "famgen#index"
 
+  #root :to => "controller#action"
+
   get 'famgen/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   match ':controller(/:action(/:id))', :via => :get
 
   get "/pages/:page" => "pages#show"
+
+   #root 'pages#home'
+
+  #scope controller: :pages do
+    #get :help
+    #get :about
+    #get :contact
+  #end
 
 end
